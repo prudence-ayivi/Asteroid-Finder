211980bb696d719a03236181ff1fcbaaa608aadc
Prudence AYIVI <ayiviprudencee@gmail.com>
2026-02-24 15:03:57 +0100

    Change NEO data indexing

--- app/api/sync-neos/route.js
+++ app/api/sync-neos/route.js
@@ -17,37 +17,41 @@ export async function GET() {
 
     const objects = data.data;
 
-    const index = {};
+    // const index = {};
+    // if (!index[date]) index[date] = [];
 
     for (const obj of objects) {
       const [spkid, full_name, pdes, name, neo, pha, diameter, e, a, q, i, om, ma, ad, n, per, per_y, classValue, first_obs, last_obs, H] = obj;
 
-      if (!index[first_obs]) {
-        index[first_obs] = [];
+      const date = first_obs; // YYYY-MM-DD
+      if (!date) return;
+
+      if (!index[date]) {
+        index[date] = [];
       }
 
-      index[first_obs].push({
-        spkid,
-        full_name,
-        pdes,
-        name,
-        neo,
-        pha,
-        diameter,
-        e,
-        a,
-        q,
-        i,
-        om,
-        ma,
-        ad,
-        n,
-        per,
-        per_y,
-        class: classValue,
-        first_obs,
-        last_obs,
-        H,
+      index[date].push({
+      spkid: obj[0],
+      full_name: obj[1],
+      pdes: obj[2],
+      name: obj[3],
+      neo: obj[4] === "Y",
+      pha: obj[5] === "Y" || obj[5] === "N",
+      diameter: obj[6],
+      e: obj[7],
+      a: obj[8],
+      q: obj[9],
+      i: obj[10], 
+      om: obj[11],
+      ma: obj[12],
+      ad: obj[13],
+      n: obj[14],
+      per: obj[15],
+      per_y: obj[16],
+      class: obj[17],
+      first_obs: date,
+      last_obs: obj[19],
+      H: obj[20],
       });
     }
 
